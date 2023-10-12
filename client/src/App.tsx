29021e682613fb8ba802579964afb5cea90969d6
Thibault Lemery <lemery.thibault.pro@gmail.com>
2023-10-12 19:52:51 +0200

    created pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import Login from './pages/Login'
+import Profile from './pages/Profile'
+import AddShow from './pages/AddShow'
+import SignUp from './pages/SignUp'
+import Shows from './pages/Shows'
 import './App.css'
+import { useState } from 'react'
 
 function App() {
+  const [auth, setAuth] = useState(false)
 
   return (
     <>
-      <div>
-        Home
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<HomePage auth={auth} />} />
+          <Route exact path="/login" element={<Login auth={auth} setAuth={setAuth}/>} />
+          <Route exact path="/profile" element={<Profile auth={auth} />} />
+          <Route exact path="/signup" element={<SignUp auth={auth} />} />
+          <Route exact path="/addshow" element={<AddShow auth={auth} />} />
+          <Route exact path="/show" element={<Shows auth={auth} />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ client/src/pages/AddShow.tsx
@@ -0,0 +1,7 @@
+export default function AddShow(){
+  return(
+    <div>
+      Add Show
+    </div>
+  )
+}
