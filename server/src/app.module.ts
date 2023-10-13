af028a59379d7a1b540a3e45ec42751c16321114
Thibault Lemery <lemery.thibault.pro@gmail.com>
2023-10-13 13:29:24 +0200

    created auth and user modules

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './interface/controllers/auth.controller';
 import { ShowsController } from './interface/controllers/shows.controller';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule],
   controllers: [AuthController, ShowsController],
   // providers: [AppService],
 })
