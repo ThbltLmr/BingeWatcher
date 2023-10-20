9d6bd05dc5b3b8c1c73a7a27f97256dc5ef78d9a
Thibault Lemery <lemery.thibault.pro@gmail.com>
2023-10-20 22:12:42 +0200

    carrousel for now

--- client/src/components/Carrousel.tsx
+++ client/src/components/Carrousel.tsx
@@ -1,5 +1,7 @@
 import { Show } from "../types";
 import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronRight, faChevronLeft } from "@fortawesome/free-solid-svg-icons";
 
 export default function Carrousel({shows}: {shows: Show[]}){
   const [startIndex, setStartIndex] = useState(0)
@@ -7,13 +9,16 @@ export default function Carrousel({shows}: {shows: Show[]}){
 
   return(
     <div>
-      <div className="flex">
-        <a>
-          
-        </a>
+      <div className="flex relative w-fit">
+        <div className="absolute left-0 text-xxl h-full w-32">
+          <a><FontAwesomeIcon icon={faChevronLeft} /></a>
+        </div>
         {shows.slice(startIndex, endIndex).map((show) => {
           return <img className="m-2 rounded" key={show.TMDBid} src={show.posterURL}></img>;
         })}
+        <div className="absolute right-0 text-xxl h-full w-32">
+          <a><FontAwesomeIcon icon={faChevronRight} /></a>
+        </div>
       </div>
     </div>
   )
