58e005ee95458ff5eff8383dcd9d7a6c3f982080
Thibault Lemery <lemery.thibault.pro@gmail.com>
2023-10-29 17:29:47 +0100

    created CRUD routes in show service

--- server/src/shows/show.service.ts
+++ server/src/shows/show.service.ts
@@ -13,11 +13,31 @@ export class ShowService {
     return this.showRepository.find();
   }
 
+  async findOne(id: number): Promise<Show> {
+    return this.showRepository.findOne({
+      where: {
+        id: id,
+      },
+    });
+  }
+
   async create(showData: any): Promise<Show> {
     const show = new Show();
     show.title = showData.title;
     show.description = showData.description;
     show.numberOfSeasons = showData.numberOfSeasons;
     return this.showRepository.save(show);
+  }
+
+  async update(id: number, showData: any): Promise<Show> {
+    const show = await this.findOne(id);
+    show.title = showData.title;
+    show.description = showData.description;
+    show.numberOfSeasons = showData.numberOfSeasons;
+    return this.showRepository.save(show);
+  }
+
+  async delete(id: number): Promise<any> {
+    return this.showRepository.delete(id);
   }
 }
