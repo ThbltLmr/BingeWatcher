c7d8250ab193ff4a35e92b08301b05afcd1e16b9
Thibault Lemery <lemery.thibault.pro@gmail.com>
2023-11-15 17:15:28 +0100

    refacto tmdb api service

--- server/src/shows/services/shows.tmdbapi.service.ts
+++ server/src/shows/services/shows.tmdbapi.service.ts
@@ -4,26 +4,31 @@ import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class TMDBAPIService {
+  tmdbAPIKey: string;
+  tmdbAPIURL: string;
   constructor(
     private configService: ConfigService,
     private readonly httpService: HttpService,
-  ) {}
+  ) {
+    this.tmdbAPIKey = this.configService.get<string>('TMDB_API_KEY');
+    this.tmdbAPIURL = this.configService.get<string>('TMDB_API_URL');
+  }
 
   // TODO: change genres_ids to genre names (string[])
   async searchShows(query: string) {
-    const tmdbAPIKey = this.configService.get<string>('TMDB_API_KEY');
-    const tmdbAPIURL = this.configService.get<string>('TMDB_API_URL');
+    // const tmdbAPIKey = this.configService.get<string>('TMDB_API_KEY');
+    // const tmdbAPIURL = this.configService.get<string>('TMDB_API_URL');
     const response = await this.httpService.axiosRef.get(
-      `${tmdbAPIURL}/search/tv?api_key=${tmdbAPIKey}&query=${query}`,
+      `${this.tmdbAPIURL}/search/tv?api_key=${this.tmdbAPIKey}&query=${query}`,
     );
     return response.data.results;
   }
 
   async getShow(tmdbId: string) {
-    const tmdbAPIKey = this.configService.get<string>('TMDB_API_KEY');
-    const tmdbAPIURL = this.configService.get<string>('TMDB_API_URL');
+    // const tmdbAPIKey = this.configService.get<string>('TMDB_API_KEY');
+    // const tmdbAPIURL = this.configService.get<string>('TMDB_API_URL');
     const response = await this.httpService.axiosRef.get(
-      `${tmdbAPIURL}/tv/${tmdbId}?api_key=${tmdbAPIKey}`,
+      `${this.tmdbAPIURL}/tv/${tmdbId}?api_key=${this.tmdbAPIKey}`,
     );
     return response.data;
   }
