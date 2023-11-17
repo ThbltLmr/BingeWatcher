783f058fc880146553806e71c8d965ada55ba555
Thibault Lemery <lemery.thibault.pro@gmail.com>
2023-11-17 13:44:39 +0100

    rewrote get all users query

--- server/src/bounded-contexts/user-registration/infrastructure/database/user-registration.repository.ts
+++ server/src/bounded-contexts/user-registration/infrastructure/database/user-registration.repository.ts
@@ -1,19 +1,26 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
-import { User } from '../../../../shared-kernel/orm-entities/user.orm-entity';
+import { UserOrmEntity } from '../../../../shared-kernel/orm-entities/user.orm-entity';
+import { UserRegistrationMapper } from './user-registration.mapper';
+import { UserEntity } from '../../domain/entities/user.entity';
 
 @Injectable()
 export class UsersService {
   constructor(
     @Inject('USER_REPOSITORY')
-    private usersRepository: Repository<User>,
+    private usersRepository: Repository<UserOrmEntity>,
+    private userRegistrationMapper: UserRegistrationMapper,
   ) {}
 
-  async findAll(): Promise<User[]> {
-    return this.usersRepository.find();
+  async findAll(): Promise<UserEntity[]> {
+    const allUserOrmEntities = await this.usersRepository.find();
+    const allUserEntities = allUserOrmEntities.map((userOrmEntity) => {
+      return this.userRegistrationMapper.toEntity(userOrmEntity);
+    });
+    return allUserEntities;
   }
 
-  async findOne(id: number): Promise<User> {
+  async findOne(id: number): Promise<UserOrmEntity> {
     return this.usersRepository.findOne({
       where: {
         id: id,
@@ -21,7 +28,7 @@ export class UsersService {
     });
   }
 
-  async findOneByUsername(username: string): Promise<User> {
+  async findOneByUsername(username: string): Promise<UserOrmEntity> {
     return this.usersRepository.findOne({
       where: {
         username: username,
@@ -29,14 +36,14 @@ export class UsersService {
     });
   }
 
-  async create(userData: any): Promise<User> {
+  async create(userData: any): Promise<UserOrmEntity> {
     const user = new User();
     user.username = userData.username;
     user.password = userData.password;
     return this.usersRepository.save(user);
   }
 
-  async update(id: number, userData: any): Promise<User> {
+  async update(id: number, userData: any): Promise<UserOrmEntity> {
     const user = await this.findOne(id);
     user.username = userData.username;
     user.password = userData.password;
