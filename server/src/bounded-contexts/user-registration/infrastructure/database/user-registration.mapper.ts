1eadf390da9aad2c72b769b2e616fe38d0356c65
Thibault Lemery <lemery.thibault.pro@gmail.com>
2023-11-17 13:39:01 +0100

    created user entity mapper

--- server/src/bounded-contexts/user-registration/infrastructure/database/user-registration.mapper.ts
+++ server/src/bounded-contexts/user-registration/infrastructure/database/user-registration.mapper.ts
@@ -1,1 +1,20 @@
-export class UserRegistrationMapper {}
+import { UserOrmEntity } from 'src/shared-kernel/orm-entities/user.orm-entity';
+import { UserEntity } from '../../domain/entities/user.entity';
+import { Email } from '../../domain/value-objects/email.value-object';
+
+export class UserRegistrationMapper {
+  toOrmEntity(user: UserEntity): UserOrmEntity {
+    const ormEntity = new UserOrmEntity();
+    ormEntity.username = user.username;
+    ormEntity.password = user.password;
+    ormEntity.email = user.email.value;
+    return ormEntity;
+  }
+  toEntity(userOrmEntity: UserOrmEntity): UserEntity {
+    const user = new UserEntity();
+    user.username = userOrmEntity.username;
+    user.password = userOrmEntity.password;
+    user.email = new Email(userOrmEntity.email);
+    return user;
+  }
+}
