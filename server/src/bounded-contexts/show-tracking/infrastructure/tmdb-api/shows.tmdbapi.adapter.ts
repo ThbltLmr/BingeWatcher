9db3730bf29ca87cde4951c77ed2228cf76c770c
Thibault Lemery <lemery.thibault.pro@gmail.com>
2023-11-28 12:31:04 +0100

    included mapper

--- server/src/bounded-contexts/show-tracking/infrastructure/tmdb-api/shows.tmdbapi.adapter.ts
+++ server/src/bounded-contexts/show-tracking/infrastructure/tmdb-api/shows.tmdbapi.adapter.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { SearchQueryDto } from '../../interface/dtos/search-query.dto';
 import { GetShowDetailsQuery } from '../queries/get-show-details.query';
+import { TmdbApiMapper } from './shows.tmdbapi.mapper';
 
 @Injectable()
 export class TmdbApiAdapter {
@@ -11,7 +12,7 @@ export class TmdbApiAdapter {
   constructor(
     private configService: ConfigService,
     private readonly httpService: HttpService,
-    priva
+    private readonly tmdbApiMapper: TmdbApiMapper,
   ) {
     this.tmdbAPIKey = this.configService.get<string>('TMDB_API_KEY');
     this.tmdbAPIURL = this.configService.get<string>('TMDB_API_URL');
@@ -23,13 +24,13 @@ export class TmdbApiAdapter {
       `${this.tmdbAPIURL}/search/tv?api_key=${this.tmdbAPIKey}&query=${query.query}`,
     );
     const results = response.data.results;
-    return results;
+    return this.tmdbApiMapper.SearchResultsToEntities(results);
   }
 
   async getShow(query: GetShowDetailsQuery) {
     const response = await this.httpService.axiosRef.get(
       `${this.tmdbAPIURL}/tv/${query.tmdbId}?api_key=${this.tmdbAPIKey}`,
     );
-    return response.data;
+    return this.tmdbApiMapper.ShowDetailsToEntity(response.data);
   }
 }
