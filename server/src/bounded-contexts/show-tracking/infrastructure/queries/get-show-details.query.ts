8c4170e7df49b758dfc9d801a51e66d0da6c9f23
Thibault Lemery <lemery.thibault.pro@gmail.com>
2023-11-29 12:10:01 +0100

    done for now

--- server/src/bounded-contexts/show-tracking/infrastructure/queries/get-show-details.query.ts
+++ server/src/bounded-contexts/show-tracking/infrastructure/queries/get-show-details.query.ts
@@ -1,3 +1,3 @@
 export class GetShowDetailsQuery {
-  tmdbId: string;
+  tmdbId: number;
 }
--- server/src/bounded-contexts/show-tracking/interface/controllers/watched-shows.controller.ts
+++ server/src/bounded-contexts/show-tracking/interface/controllers/watched-shows.controller.ts
@@ -3,6 +3,15 @@ import { AuthGuard } from '../../../../shared-kernel/authentication/guards/auth.
 import { AuthService } from '../../../../shared-kernel/authentication/services/auth.service';
 import { WatchedShowsRepository } from '../../infrastructure/database/watched-shows.repository';
 import { WatchedShowDataDto } from '../dtos/watched-show-data.dto';
+import { Repository } from 'typeorm';
+import { UserOrmEntity } from 'src/shared-kernel/orm-entities/user.orm-entity';
+import { UsersMapper } from '../../infrastructure/database/users.mapper';
+import { ShowOrmEntity } from 'src/shared-kernel/orm-entities/show.orm-entity';
+import { ShowsRepository } from '../../infrastructure/database/shows.repository';
+import { CreateShowCommand } from '../../infrastructure/commands/create-show.command';
+import { TmdbApiAdapter } from '../../infrastructure/tmdb-api/shows.tmdbapi.adapter';
+import { CreateWatchedShowCommand } from '../../infrastructure/commands/create-watched-show.command';
+import { ShowTrackingMapper } from '../../infrastructure/database/shows.mapper';
 
 // TODO
 @Controller('watchedshows')
@@ -10,16 +19,62 @@ export class WatchedShowsController {
   constructor(
     private authService: AuthService,
     private watchedShowsSRepository: WatchedShowsRepository,
+    private usersMapper: UsersMapper,
+    private usersOrmRepository: Repository<UserOrmEntity>,
+    private showsOrmRepository: Repository<ShowOrmEntity>,
+    private showsRepository: ShowsRepository,
+    private tmdbApiAdapter: TmdbApiAdapter,
+    private showsMapper: ShowTrackingMapper,
   ) {}
 
   @UseGuards(AuthGuard)
   @Post('add')
-  addWatchedShow(@Request() req, @Body() watchedShowData: WatchedShowDataDto) {
+  async addWatchedShow(
+    @Request() req,
+    @Body() watchedShowData: WatchedShowDataDto,
+  ) {
     const userId = req.user.sub;
+    const userOrmEntity = await this.usersOrmRepository.findOne({
+      where: {
+        id: userId,
+      },
+    });
+    const userEntity = this.usersMapper.toEntity(userOrmEntity);
+    let show = await this.showsOrmRepository.findOne({
+      where: {
+        tmdbId: watchedShowData.tmdbId,
+      },
+    });
+
+    if (!show) {
+      const createShowCommand: CreateShowCommand =
+        await this.createShowCommandFromTmdbId(watchedShowData.tmdbId);
+      const showOrmEntity =
+        await this.showsRepository.create(createShowCommand);
+      show = this.showsMapper.toEntity(showOrmEntity);
+    }
+
+    const createWatchedShowCommand: CreateWatchedShowCommand = {
+      user: userEntity,
+      show: show,
+      numberOfSeasonsWatched: watchedShowData.numberOfSeasonsWatched,
+    };
+
     return this.watchedShowsSRepository.addWatchedShow(
       userId,
       watchedShowData.tmdbId,
       watchedShowData.numberOfSeasonsWatched,
     );
+  }
+
+  private async createShowCommandFromTmdbId(
+    tmdbId: number,
+  ): Promise<CreateShowCommand> {
+    const showDetails = await this.tmdbApiAdapter.getShow({
+      tmdbId: tmdbId,
+    });
+    let createShowCommand = new CreateShowCommand();
+    createShowCommand = { ...createShowCommand, ...showDetails };
+    return createShowCommand;
   }
 }
