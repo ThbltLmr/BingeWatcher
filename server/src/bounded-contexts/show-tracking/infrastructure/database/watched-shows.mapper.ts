d6b3501e58811db78d3573e2e345c0de44ae48b8
Thibault Lemery <lemery.thibault.pro@gmail.com>
2023-11-29 11:40:32 +0100

    created mapper for watched shows

--- server/src/bounded-contexts/show-tracking/infrastructure/database/watched-shows.mapper.ts
+++ server/src/bounded-contexts/show-tracking/infrastructure/database/watched-shows.mapper.ts
@@ -1,9 +1,37 @@
-// import { WatchedShowOrmEntity } from 'src/shared-kernel/orm-entities/watched-show.orm-entity';
-// import { WatchedShowEntity } from '../../domain/entities/watched-show.entity';
+import { WatchedShowOrmEntity } from 'src/shared-kernel/orm-entities/watched-show.orm-entity';
+import { WatchedShowEntity } from '../../domain/entities/watched-show.entity';
+import { Injectable } from '@nestjs/common';
+import { UsersMapper } from './users.mapper';
+import { ShowTrackingMapper } from './shows.mapper';
 
+@Injectable()
 export class WatchedShowsMapper {
-  // toEntity(watchedShowOrmEntity: WatchedShowOrmEntity): WatchedShowEntity {
-  //   TODO
-  //   return new WatchedShowEntity();
-  // }
+  constructor(
+    private usersMapper: UsersMapper,
+    private showsMapper: ShowTrackingMapper,
+  ) {}
+  toEntity(watchedShowOrmEntity: WatchedShowOrmEntity): WatchedShowEntity {
+    const watchedShowEntity = new WatchedShowEntity();
+    watchedShowEntity.id = watchedShowOrmEntity.id;
+    watchedShowEntity.user = this.usersMapper.toEntity(
+      watchedShowOrmEntity.user,
+    );
+    watchedShowEntity.show = this.showsMapper.toEntity(
+      watchedShowOrmEntity.show,
+    );
+    watchedShowEntity.seasonsWatched = watchedShowOrmEntity.watchedSeasons;
+    return watchedShowEntity;
+  }
+  toOrmEntity(watchedShowEntity: WatchedShowEntity): WatchedShowOrmEntity {
+    const watchedShowOrmEntity = new WatchedShowOrmEntity();
+    watchedShowOrmEntity.id = watchedShowEntity.id;
+    watchedShowOrmEntity.user = this.usersMapper.toOrmEntity(
+      watchedShowEntity.user,
+    );
+    watchedShowOrmEntity.show = this.showsMapper.toOrmEntity(
+      watchedShowEntity.show,
+    );
+    watchedShowOrmEntity.watchedSeasons = watchedShowEntity.seasonsWatched;
+    return watchedShowOrmEntity;
+  }
 }
