83817c33ed9e10b03460410352e773cf686ac50f
Thibault Lemery <lemery.thibault.pro@gmail.com>
2023-12-19 18:03:00 +0100

    stuff

--- client/src/components/CenterPopUp.tsx
+++ client/src/components/CenterPopUp.tsx
@@ -1,10 +1,14 @@
-import { Button } from "@mui/material";
+import { Button, FormControlLabel, Radio, RadioGroup } from "@mui/material";
 import { Show } from "../types";
 import ShowCard from "./ShowCard";
 import Slider from '@mui/material/Slider';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import { useState } from "react";
 
 export default function CenterPopUp({show}: {show: Show}){
+
+  const [addButtonOpen, setAddButtonOpen] = useState(false);
+  const [sliderOpen, setSliderOpen] = useState(false);
 
   function valuetext(value: number) {
     if (value === 1) return `${value} season`;
@@ -12,13 +16,37 @@ export default function CenterPopUp({show}: {show: Show}){
     return `${value} seasons`;
   }
 
+  const handleStartedShow = () => {
+    setAddButtonOpen(true);
+    setSliderOpen(true);
+  }
+  const handleNotStartedShow = () => {
+    setAddButtonOpen(true);
+    setSliderOpen(false);
+  }
+
   return(
     <div className="absolute h-screen w-screen bg-black bg-opacity-70">
-      <div className="absolute rounded w-fit h-fit inset-x-1/2 inset-y-1/3 -translate-x-1/2 -translate-y-1/2 p-3 bg-slate-200">
+      <div className="absolute rounded mt-8 w-fit h-fit inset-x-1/2 inset-y-1/3 -translate-x-1/2 -translate-y-1/2 p-3 bg-slate-200">
         <div className="flex flex-col m-2 w-96">
           <ShowCard show={show} />
         </div>
-        <div className="m-4">
+        <div className="mt-4 mb-4">
+          <h2 className="text-lg text-center">Have you started this show?</h2>
+        </div>
+        <RadioGroup
+          row
+          aria-labelledby="demo-row-radio-buttons-group-label"
+          name="row-radio-buttons-group"
+          >
+         <div className="mx-auto">
+            <FormControlLabel value="yes"  onClick={handleStartedShow} control={<Radio />} label="Yes" />
+            <FormControlLabel value="no" onClick={handleNotStartedShow} control={<Radio />} label="No" />
+         </div>
+        </RadioGroup>
+       {sliderOpen &&
+       <div>
+       <div className="mt-4 mb-12">
           <h2 className="text-lg text-center">How many seasons have you watched?</h2>
         </div>
         <div className="flex align-middle flex-col">
@@ -34,10 +62,15 @@ export default function CenterPopUp({show}: {show: Show}){
             sx={{width: '80%', margin: 'auto'}}
             valueLabelFormat={(value) => valuetext(value)}
           />
-          <Button variant="contained" endIcon={<AddCircleOutlineIcon />} sx={{width: '80%', margin: 'auto'}}>
-              Add this show
-          </Button>
         </div>
+        </div>
+        }
+        {addButtonOpen &&
+        <div className="flex flex-col align-middle">
+        <Button variant="contained" endIcon={<AddCircleOutlineIcon />} sx={{width: '80%', margin: 'auto'}}>
+            Add this show
+        </Button>
+        </div>}
       </div>
     </div>
   )
